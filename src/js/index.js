67fb6fabc9c2e34998503e86b280a6de012b4900
mariaisabeloliveira <166269243+mariaisabeloliveira@users.noreply.github.com>
2024-04-07 18:38:00 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoMostrarMenos = document.querySelector('.btn-mostrar-menos');
+const projetosAtivos = document.querySelectorAll('.projeto')
 
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
     esconderBotao();
+    BotaoMostrarMenos();
+});
+
+botaoMostrarMenos.addEventListener('click', () => {
+    BotaoMostrarMenos();
+    esconderMostrarMenos();
+    esconderProjetos();
+    mostrarBotaoMostrarProjetos();
 });
 
 function mostrarMaisProjetos() {
@@ -12,6 +22,26 @@ function mostrarMaisProjetos() {
     });
 }
 
-function esconderBotao() {
-    botaoMostrarProjetos.classList.add('remover');
+if (projetosAtivos.length>4) {
+    botaoMostrarProjetos.classList.remove('remover');
+}
+
+function esconderProjetos() {
+    if (projetosAtivos.length > 4) {
+        for (let i = 4; i < projetosAtivos.length; i++) {
+            projetosAtivos[i].classList.remove('ativo');
+        }
+    }
+}
+
+function esconderMostrarMenos() {
+    botaoMostrarMenos.classList.add('remover')
+}
+
+function BotaoMostrarMenos() {
+    botaoMostrarMenos.classList.remove('remover');
+}
+
+function mostrarBotaoMostrarProjetos() {
+    botaoMostrarProjetos.classList.remove('remover');
 }
